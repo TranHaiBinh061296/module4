b3b3f7da98730c3e9d8beca4db2af49535851edf
Hinh Bai <haibinh.061296@gmail.com>
2022-11-13 21:20:21 +0700

    customer management

---
+++ 11.WebServicevàRESTful/customer-management/src/main/java/com/codegym/cms/model/Customer.java
@@ -0,0 +1,46 @@
+package com.codegym.cms.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
