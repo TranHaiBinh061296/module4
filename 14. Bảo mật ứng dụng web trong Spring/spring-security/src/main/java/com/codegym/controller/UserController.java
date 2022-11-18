28dd3e5befdf552fc465d51fb36e3b2a54790225
Hinh Bai <haibinh.061296@gmail.com>
2022-11-19 00:12:51 +0700

    security

---
+++ 14. Bảo mật ứng dụng web trong Spring/spring-security/src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.codegym.controller;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("/index");
+    }
+
+    @GetMapping("/user")
+    public ModelAndView user(Principal principal) {
+        System.out.println(principal.getName());
+        return new ModelAndView("/user");
+    }
+
+    @GetMapping("/admin")
+    public ModelAndView admin() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        System.out.println(context.getAuthentication().getName());
+        return new ModelAndView("/admin");
+    }
+}
---
+++ 14. Bảo mật ứng dụng web trong Spring/spring-security/src/main/java/com/codegym/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.codegym.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
