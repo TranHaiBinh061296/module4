2c90892b9a8d9299ef9eca6272b127124285ea49
Hinh Bai <haibinh.061296@gmail.com>
2022-11-19 12:08:15 +0700

    login

---
+++ 15. Phân quyền ứng dụng/spring-role-based-authorization/src/main/java/com/codegym/configuration/AppInit.java
@@ -0,0 +1,22 @@
+package com.codegym.configuration;
+
+import com.codegym.security.SecurityConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
