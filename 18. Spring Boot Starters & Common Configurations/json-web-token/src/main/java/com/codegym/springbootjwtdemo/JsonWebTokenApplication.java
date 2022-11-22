9a8816b38d4ca2d9f24458324c40397b96d79166
Hinh Bai <haibinh.061296@gmail.com>
2022-11-22 09:15:33 +0700

    update

---
+++ 18. Spring Boot Starters & Common Configurations/json-web-token/src/main/java/com/codegym/springbootjwtdemo/JsonWebTokenApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.springbootjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWebTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonWebTokenApplication.class, args);
+    }
+
+}
